777f6011ed87d7c5557f117aa96dedd720513e96
ELrhali <mohammedelrhali98@gmail.com>
2023-03-15 20:04:58 +0100

    new 2

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 from flask import Flask, request, jsonify
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-import numpy as np
-import cv2
+
 
 app = Flask(__name__)
 
